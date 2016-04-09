b3d90dc0f4bdd85b8f6e1a5df3465f23a211b2df
Daniel Lau <danielcplau@gmail.com>
2016-04-09 14:06:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 $(window).scroll(function(){
-    $(".galaxy").css("opacity", 1 - $(window).scrollTop() / 1900);
+    $(".stars").css("opacity", 1 - $(window).scrollTop() / 1900);
+});
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
 });
