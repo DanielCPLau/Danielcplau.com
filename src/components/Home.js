411e54dcbe1197357a38f8a5fa5b7b3dd5d8d52f
Daniel Lau <danielcplau@gmail.com>
2016-12-29 14:32:40 -0800

    New homepage image, navbar is too light though, will fix later

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
     };
 		return (
 			<div>
-				<div style={bg}><img style={img} src='https://raw.githubusercontent.com/DanielCPLau/Danielcplau.com/gh-pages/Site%201.0/Images/1506271_642286765832258_692446235_o.jpg'/></div>
+				<div style={bg}><img style={img} src='https://raw.githubusercontent.com/DanielCPLau/Danielcplau.com/redesign/Site%201.0/Images/san-francisco-52.jpg'/></div>
 				<div style={sigStyle}><Signature/></div>
 			</div>
 		)
