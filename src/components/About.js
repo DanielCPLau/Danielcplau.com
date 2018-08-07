b87fe568d850ec2e044a2c6d542a06f88071855a
Daniel Lau <danielcplau@gmail.com>
2018-08-07 18:23:55 -0400

    Quick update

--- src/components/About.js
+++ src/components/About.js
@@ -24,8 +24,9 @@ class About extends Component {
         <div>
           <div style={textStyle}>
             Hey, my name is Daniel. <br/>
-            I'm nearly done with college. (Computer Science B.S. UCSD 19') <br/> <br/>
-            This site gets random updates as time passes. <br/>
+            I'm currently taking a break to do a couple internships before wrapping up college. <br/>
+            (Computer Science B.S. UCSD (Spring 2019) <br/> <br/>
+            This site gets random updates as time passes (totally due for an entire rewrite when I find the time). <br/>
             <br/>
             Feel free to shoot me a message!
           </div>
